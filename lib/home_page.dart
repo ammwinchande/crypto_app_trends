8023de2310645068d18c230adc7ac51904aeaf1a
Muhammad A. Mwinchande <ammwinchande@gmail.com>
2020-03-01 22:02:53 +0300

    implemented the presenter for completing the mvp

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,19 +1,29 @@
+import 'package:crypto_app/src/models/crypto.dart';
+import 'package:crypto_app/src/presenters/contracts/crypto_list_view_contract.dart';
+import 'package:crypto_app/src/presenters/crypto_list_presenter.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  final List currencies;
-  HomePage({Key key, this.currencies}) : super(key: key);
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> implements CryptoListViewContract {
+  CryptoListPresenter _cryptoListPresenter;
+
+  bool _isLoading;
+  List<Crypto> _currencies;
   final List<Color> _colors = [Colors.blue, Colors.red, Colors.green];
+
+  _HomePageState() {
+    _cryptoListPresenter = new CryptoListPresenter(this);
+  }
 
   @override
   void initState() {
     super.initState();
+    _isLoading = true;
+    _cryptoListPresenter.loadCurrencies();
   }
 
   @override
@@ -30,41 +40,43 @@ class _HomePageState extends State<HomePage> {
     return Container(
       child: Column(
         children: <Widget>[
-          Flexible(
-            child: ListView.builder(
-              itemCount: widget.currencies.length,
-              itemBuilder: (BuildContext context, int index) {
-                final Map currency = widget.currencies[index];
-                final Color color = _colors[index % _colors.length];
+          _isLoading
+              ? Center(child: CircularProgressIndicator())
+              : Flexible(
+                  child: ListView.builder(
+                    itemCount: _currencies.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final Crypto currency = _currencies[index];
+                      final Color color = _colors[index % _colors.length];
 
-                return itemTile(currency: currency, color: color);
-              },
-            ),
-          ),
+                      return itemTile(currency: currency, color: color);
+                    },
+                  ),
+                ),
         ],
       ),
     );
   }
 
-  Padding itemTile({Map currency, Color color}) {
+  Padding itemTile({Crypto currency, Color color}) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 16.0),
       child: ListTile(
         leading: CircleAvatar(
           backgroundColor: color,
-          child: Text(currency['symbol']),
+          child: Text(currency.symbol),
           radius: 50,
         ),
         title: Padding(
           padding: const EdgeInsets.only(top: 16.0),
           child: Text(
-            currency['name'].toString().toUpperCase(),
+            currency.name.toUpperCase(),
             style: TextStyle(fontWeight: FontWeight.bold),
           ),
         ),
         subtitle: subTitleText(
-          priceUsd: currency['price_usd'] + '\n',
-          percentage: currency['percent_change_1h'],
+          priceUsd: currency.priceUsd + '\n',
+          percentage: currency.percentChange1h,
         ),
       ),
     );
@@ -92,5 +104,19 @@ class _HomePageState extends State<HomePage> {
     return RichText(
       text: TextSpan(children: [priceText, percentageChangeText]),
     );
+  }
+
+  @override
+  void onLoadComplete(List<Crypto> cryptos) {
+    setState(() {
+      _currencies = cryptos;
+      print(_currencies);
+    });
+    _isLoading = false;
+  }
+
+  @override
+  void onLoadError() {
+    // TODO: implement onLoadError
   }
 }
